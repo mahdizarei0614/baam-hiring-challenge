8c5fbd0a2d15b212ffd524e0cbc940de4d76c331
skyBlue <mahdizarei0614@gmail.com>
2022-10-27 05:52:22 +0330

    - sync environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gateway.marvel.com/',
-  apiKey: ''
+  apiUrl: 'https://gateway.marvel.com/v1/public/',
+  apiPublicKey: 'e43aa9378aa074afa9e8f95822e80194',
+  apiPrivateKey: '6116b299f0e4016da2154b32ae74d58a172f0d1c'
 };
